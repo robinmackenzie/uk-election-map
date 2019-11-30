2a395dad588333d2992815ed3085f4e62af2ca5a
Robin Mackenzie <rtmackenzie@gmail.com>
2019-12-01 10:36:46 +1100

    Update click behaviour to work with results buttons

--- app.js
+++ app.js
@@ -55,6 +55,11 @@ function colorMap() {
       return d.properties.color;
     });
   
+  // update info
+  const currentId = config["selectionData"]["Id"];
+  config["selectionData"] = config[config["dataset"]].find(k => k.Id === currentId);
+  updateInfoPanelHtml(config["selectionData"]);
+  
 }
 
 function initMap() {
@@ -129,8 +134,8 @@ function initMap() {
 
     // update info div
     info.attr("class", "infoPanel");
-    
-    updateInfoPanelHtml(d.properties["electionData"]);
+    config["selectionData"] = d.properties["electionData"];
+    updateInfoPanelHtml(config["selectionData"]);
   }
     
   function doubleClicked(d) {
@@ -142,107 +147,110 @@ function initMap() {
       newWindow = window.open(link, "_blank");
       newWindow.focus();
     }
-  }
-    
-  function updateInfoPanelHtml(data) {
-
-    info.select("#constituencyName")
-      .text(data.Summary.Constituency);
-    info.select("#winningCandidateName")
-      .text("Winner: " + data.Summary.WinningCandidate);
-    info.select("#winningPartyName")
-      .text("Winner: " + data.Summary.WinningPartyName);
-    info.select("#partyColourBlock")
-      .style("background-color", data.Summary.PartyColour)
-      .style("height", "5px")
-      .style("width", "100%");
-    info.select("#electorate")
-      .text("Electorate: " + data.Summary.Electorate.toLocaleString("en-uk"));
-    info.select("#turnout")
-      .text("Turnout: " + data.Summary.ValidVotes.toLocaleString("en-uk"));
-    info.select("#votes")
-      .text("Votes for winner: " + data.Summary.WinningVoteCount.toLocaleString("en-uk"));
-    info.select("#share")
-      .text("Winning share: " + data.Summary.ValidVotePercent.toLocaleString("en-uk", {"style": "percent"}));
-
-    // votes bar chart
-    // amended from https://bl.ocks.org/alandunning/7008d0332cc28a826b37b3cf6e7bd998
-    // and https://bl.ocks.org/mbostock/7341714
-
-    // get voting data
-    var votesRaw = data.CandidateVoteInfo.map(function(k) {
-      return {
-        "party": k.PartyAbbrevTransformed, 
-        "votes": k.Votes, 
-        "color": k.PartyColour 
-      };
-    });
-    // sum over duplicate keys e.g. OTH
-    var votes = [];
-    var parties = [];
-    votesRaw.forEach(function(item, index) {
-      if (parties.indexOf(item.party) < 0) {
-        parties.push(item.party);
-        votes.push(item);
-      } else {
-        votes.filter(function(k) {
-          return k.party == item.party;
-        })[0].votes += item.votes;
-      }
-    });
-    // sort ascending
-    votes.sort(function(a, b) { return a.votes - b.votes; });
-
-    var margin = {top: 5, left: 40},
-      width = 150,
-      height = votes.length * 30; 
-
-    var x = d3.scaleLinear().range([0, width]);
-    var y = d3.scaleBand().range([height, 0]);
-
-    // remove old g from chart svg
-    info.select("#svgVotes")
-      .select("g")
-      .remove();
-    
-    // add new g to chart svg
-    var g = info 
-      .select("#svgVotes") 
-      .attr("height", height + 20)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // set up axes
-    x.domain([0, d3.max(votes, function(d) { return d.votes; })]);
-    y.domain(votes.map(function(d) { return d.party; })).padding(0.1);
-
-    // append new g to chart svg
-    g.append("g")
-      .attr("class", "y axis")
-      .call(d3.axisLeft(y));
-
-    // add bars per votes data using party color
-    // set up a g element for each entry in votes array
-    var bars = g.selectAll(".bar")
-      .data(votes)
-      .enter();
-      
-    // append a rect for each g and set color, x, y, height and width
-    bars.append("rect")
-      .style("fill", function(d) {
-        return d.color;
-      })
-      .attr("x", 0)
-      .attr("height", y.bandwidth() )
-      .attr("y", function(d) { return y(d.party); })
-      .attr("width", function(d) { return x(d.votes); });
-
-    // append a label just after rect with vote count
-    bars.append("text")
-      .attr("x", function(d) { return x(d.votes) + 4; })
-      .attr("y", function(d, i) { return y(d.party) + (y.bandwidth() / 2); })
-      .attr("dy", ".35em")
-      .text(function(d) { return d.votes.toLocaleString("en-uk"); });
-
-  }
+  }   
 }
+
+function updateInfoPanelHtml(data) {
+
+  const info = d3.select("#map div.infoPanel")
+
+  info.select("#constituencyName")
+    .text(data.Summary.Constituency);
+  info.select("#winningCandidateName")
+    .text("Winner: " + data.Summary.WinningCandidate);
+  info.select("#winningPartyName")
+    .text("Winner: " + data.Summary.WinningPartyName);
+  info.select("#partyColourBlock")
+    .style("background-color", data.Summary.PartyColour)
+    .style("height", "5px")
+    .style("width", "100%");
+  info.select("#electorate")
+    .text("Electorate: " + data.Summary.Electorate.toLocaleString("en-uk"));
+  info.select("#turnout")
+    .text("Turnout: " + data.Summary.ValidVotes.toLocaleString("en-uk"));
+  info.select("#votes")
+    .text("Votes for winner: " + data.Summary.WinningVoteCount.toLocaleString("en-uk"));
+  info.select("#share")
+    .text("Winning share: " + data.Summary.ValidVotePercent.toLocaleString("en-uk", {"style": "percent"}));
+
+  // votes bar chart
+  // amended from https://bl.ocks.org/alandunning/7008d0332cc28a826b37b3cf6e7bd998
+  // and https://bl.ocks.org/mbostock/7341714
+
+  // get voting data
+  var votesRaw = data.CandidateVoteInfo.map(function(k) {
+    return {
+      "party": k.PartyAbbrevTransformed, 
+      "votes": k.Votes, 
+      "color": k.PartyColour 
+    };
+  });
+  // sum over duplicate keys e.g. OTH
+  var votes = [];
+  var parties = [];
+  votesRaw.forEach(function(item, index) {
+    if (parties.indexOf(item.party) < 0) {
+      parties.push(item.party);
+      votes.push(item);
+    } else {
+      votes.filter(function(k) {
+        return k.party == item.party;
+      })[0].votes += item.votes;
+    }
+  });
+  // sort ascending
+  votes.sort(function(a, b) { return a.votes - b.votes; });
+
+  var margin = {top: 5, left: 40},
+    width = 150,
+    height = votes.length * 30; 
+
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleBand().range([height, 0]);
+
+  // remove old g from chart svg
+  info.select("#svgVotes")
+    .select("g")
+    .remove();
+  
+  // add new g to chart svg
+  var g = info 
+    .select("#svgVotes") 
+    .attr("height", height + 20)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // set up axes
+  x.domain([0, d3.max(votes, function(d) { return d.votes; })]);
+  y.domain(votes.map(function(d) { return d.party; })).padding(0.1);
+
+  // append new g to chart svg
+  g.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(y));
+
+  // add bars per votes data using party color
+  // set up a g element for each entry in votes array
+  var bars = g.selectAll(".bar")
+    .data(votes)
+    .enter();
+    
+  // append a rect for each g and set color, x, y, height and width
+  bars.append("rect")
+    .style("fill", function(d) {
+      return d.color;
+    })
+    .attr("x", 0)
+    .attr("height", y.bandwidth() )
+    .attr("y", function(d) { return y(d.party); })
+    .attr("width", function(d) { return x(d.votes); });
+
+  // append a label just after rect with vote count
+  bars.append("text")
+    .attr("x", function(d) { return x(d.votes) + 4; })
+    .attr("y", function(d, i) { return y(d.party) + (y.bandwidth() / 2); })
+    .attr("dy", ".35em")
+    .text(function(d) { return d.votes.toLocaleString("en-uk"); });
+
+}
+
